ac1b3868e6b55535baf4803617f03a572bc35a2d
esthergithub <esther.mugo@africascan.com>
2021-06-05 18:40:57 +0200

    hh

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module FacebookClone
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.time_zone = 'Lisbon'
+    config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
 
     # Settings in config/environments/* take precedence over those specified here.
