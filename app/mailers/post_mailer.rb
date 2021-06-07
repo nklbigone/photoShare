ec404ba3b141480e45cd3193239b6185458fe4e9
esthergithub <esther.mugo@africascan.com>
2021-06-07 09:51:08 +0200

    send email

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -2,7 +2,7 @@ class PostMailer < ApplicationMailer
 
   def post_mail(post)
   @post = post
-  mail to: "nadinantire@gmail.com", subject: "Inquiry confirmation email"
+  mail to: "esther@gmail.com", subject: "Inquiry confirmation email"
 end
 
 end
