5f03cf90e2c3c22b4b925c2e21a4397ce9e8ddbf
esthergithub <esther.mugo@africascan.com>
2021-06-07 08:51:29 +0200

    well

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -1,6 +1,6 @@
 class PostMailer < ApplicationMailer
 
-  def example(post)
+  def post_mail(post)
   @post = post
   mail to: "nadinantire@gmail.com", subject: "Inquiry confirmation email"
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
   config.eager_load = false
 
   # Show full error reports.
-  config.consider_all_requests_local = false
+  config.consider_all_requests_local = true
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
@@ -51,6 +51,7 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+  config.action_mailer.perform_deliveries = true
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
@@ -59,5 +60,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
-config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.delivery_method = :letter_opener_web
 end
